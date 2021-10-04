e4383035e28f56005f7ef4a8489d90615ce10087
zibro <jgh6272@naver.com>
2021-10-04 20:13:51 +0900

    21/10/04 브랜치 병합

--- app/src/main/java/com/example/itsapp/view/fragment/IssueFragment.kt
+++ app/src/main/java/com/example/itsapp/view/fragment/IssueFragment.kt
@@ -7,20 +7,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.itsapp.R
+import com.example.itsapp.view.adapter.NewsAdapter
+import com.example.itsapp.viewmodel.NewsViewModel
 import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.fragment_blog.*
 import kotlinx.android.synthetic.main.fragment_issue.*
+import kotlinx.android.synthetic.main.fragment_issue.tips_tab
 
 class IssueFragment : Fragment() {
-
+    private val viewModel: NewsViewModel by viewModels()
+    private lateinit var newsRecyclerView: RecyclerView
     companion object{
         const val TAG : String = "로그"
         fun newInstance() : IssueFragment{
             return IssueFragment()
         }
-        lateinit var rankFragment: RankFragment
-        lateinit var newsFragment: NewsFragment
-        lateinit var blogFragment: BlogFragment
     }
 
     // 메모리에 올라갔을때
@@ -37,31 +43,55 @@ class IssueFragment : Fragment() {
     // 뷰가 생성됐을 때
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_issue,container,false)
-        rankFragment = RankFragment.newInstance()
-        activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.today_container,rankFragment)?.commit()
 
         return view
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        today_story_tabs.addOnTabSelectedListener(object:TabLayout.OnTabSelectedListener{
+        recyclerView(view)
+        LiveData(view)
+        btnEvent(view)
+    }
+    fun LiveData(view:View){
+        viewModel.newsLiveData.observe(viewLifecycleOwner, Observer {
+            var result = it.items
+            val mAdapter = this!!.activity?.let { it1 -> NewsAdapter(result, it1) }
+            newsRecyclerView.adapter = mAdapter
+        })
+    }
+    fun recyclerView(view:View){
+        newsRecyclerView = view.findViewById(R.id.news_rv)
+        var NewsLayoutManager = LinearLayoutManager(view.context, LinearLayoutManager.VERTICAL,false)
+        newsRecyclerView.layoutManager = NewsLayoutManager
+        viewModel.searchReadNews("맥북",1,100)
+    }
+    fun btnEvent(view: View){
+        tips_tab.addOnTabSelectedListener(object: TabLayout.OnTabSelectedListener{
             override fun onTabReselected(p0: TabLayout.Tab?) {}
             override fun onTabUnselected(p0: TabLayout.Tab?) {}
             override fun onTabSelected(p0: TabLayout.Tab?) {
                 val position = p0?.position
                 when(position){
                     0 -> {
-                        rankFragment = RankFragment.newInstance()
-                        activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.today_container,rankFragment)?.commit()
+                        viewModel.searchReadNews("맥북",1,100)
                     }
-                    1->{
-                        newsFragment = NewsFragment.newInstance()
-                        activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.today_container,newsFragment)?.commit()
+                    1 -> {
+                        viewModel.searchReadNews("삼성 노트북",1,100)
+                    }
+                    2 -> {
+                        viewModel.searchReadNews("LG 노트북",1,100)
+                    }
+                    3 -> {
+                        viewModel.searchReadNews("ASUS 노트북",1,100)
+                    }
+                    4 -> {
+                        viewModel.searchReadNews("DELL 노트북",1,100)
+                    }
+                    5 -> {
+                        viewModel.searchReadNews("LENOVO 노트북",1,100)
                     }
                 }
             }
-
         })
     }
 }
